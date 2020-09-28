cd95290e89e9cae4bd6be503587595416f80f725
Sam Maus <samueldmaus@gmail.com>
2020-09-27 22:51:42 -0500

    created window for the game and put title on window

--- HelloSFML/Main.cpp
+++ HelloSFML/Main.cpp
@@ -1,1 +1,25 @@
+#include "ball.h"
+#include "bat.h"
+#include <sstream>
+#include <cstdlib>
+#include <SFML/Graphics.hpp>
 
+using namespace sf;
+
+int main()
+{
+	// creates window to view the game
+	int windowWidth = 1024;
+	int windowHeight = 768;
+	RenderWindow window(VideoMode(windowWidth, windowHeight), "PONG");
+	int score = 0;
+	int lives = 3;
+	Bat bat(windowWidth / 2, windowHeight - 20);
+	Ball ball(windowWidth / 2, 1);
+	Text hud;
+	Font font;
+	font.loadFromFile("game-over.tff");
+	hud.setFont(font);
+	hud.setCharacterSize(75);
+	hud.setFillColor(sf::Color::White);
+}
